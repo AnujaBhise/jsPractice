5a265499f3c14692a71cbc1fab80b786f958787b
Anuja_Bhise <96904529+LearnerAnuja@users.noreply.github.com>
2023-10-05 17:15:53 +0000

    Datatypes ECMA

---
+++ 01_basics/02_datatypes_ECMA_Standards.js
@@ -0,0 +1,33 @@
+"use strict"; //treat all JS code as newer version
+
+//alert("Hello")
+//alert( 5 + 6 ) //we are using nodejs not browser ,therefore error is given
+
+//1.Code readability => Should be always high
+//console.log(3+3) console.log("Anuja") //poor code readability
+console.log(3 + 3)
+console.log("Anuja")
+
+// datatypes
+let name = "Anuja" //string
+let age = 23 // number
+let isLoggedIn  = false //boolean
+
+//numbers => range is from 1 to 2 to power 53
+//bigint
+
+//string : "double quotes" or 'single quotes' allowed
+//boolean : true or false
+
+//null : standalone value 
+//undefined : value is not defined
+//let state; value of state undefined
+//null is representation of empty value.it is object
+//let temp = null;
+
+//symbol:  unique
+//object
+
+console.log(typeof "anuja");
+console.log(typeof null);
+console.log(typeof undefined);
