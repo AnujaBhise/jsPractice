18e5eef294d26365f7d32058d65097c032cbdf65
Anuja_Bhise <96904529+LearnerAnuja@users.noreply.github.com>
2023-10-05 18:01:38 +0000

    operations

--- 01_basics/03_datatype_conversion.js
+++ 01_basics/03_datatype_conversion.js
@@ -4,39 +4,39 @@ let score3 = null
 let score4 = undefined
 let score5 = "Anuja" 
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
-let valueInNumber  = Number (score2)
-console.log(typeof valueInNumber)
-console.log(valueInNumber)
+// let valueInNumber  = Number (score2)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
 
-let valueInNumber2  = Number (score3)
-console.log(typeof valueInNumber2)
-console.log(valueInNumber2)
+// let valueInNumber2  = Number (score3)
+// console.log(typeof valueInNumber2)
+// console.log(valueInNumber2)
 
-let valueInNumber3 = Number (score4)
-console.log(typeof valueInNumber3)
-console.log(valueInNumber3)
-let valueInNumber4 = Number (score5)
-console.log(typeof valueInNumber4)
-console.log(valueInNumber4)
+// let valueInNumber3 = Number (score4)
+// console.log(typeof valueInNumber3)
+// console.log(valueInNumber3)
+// let valueInNumber4 = Number (score5)
+// console.log(typeof valueInNumber4)
+// console.log(valueInNumber4)
 // "33" => 33
 // "33abc" => NaN
 //false=>0, true =>1
 
 
-let isLoggedIn =1 
-let booleanIsLoggedIn =  Boolean (isLoggedIn)
-console.log(booleanIsLoggedIn);
+// let isLoggedIn =1 
+// let booleanIsLoggedIn =  Boolean (isLoggedIn)
+// console.log(booleanIsLoggedIn);
 
 let emptyString = ""
-let empStr = Boolean (emptyString)
-console.log( empStr)
+// let empStr = Boolean (emptyString)
+// console.log( empStr)
 
-let myname = "Anuja"
-let name1 = Boolean( myname)
-console.log(name1)
+// let myname = "Anuja"
+// let name1 = Boolean( myname)
+// console.log(name1)
 
 //1 => true
 //0 => false
@@ -44,6 +44,36 @@ console.log(name1)
 //"anuja" => true
 
 let someNum = 33
-let strNumber = String (someNum)
-console.log(strNumber )
-console.log(typeof strNumber)
+// let strNumber = String (someNum)
+// console.log(strNumber )
+// console.log(typeof strNumber)
+
+/******************OPERATIONS******************/
+let value = 3
+let negValue = -value 
+// console.log(negValue)
+
+let str1= "Hello "
+let str2 = "Anuja"
+let str3 = str1 + str2 
+// console.log(str3)
+
+// console.log("1"+2); //12
+// console.log(1+"2"); //12
+// console.log("1"+ 2 + 2); //122
+// console.log(1 + 2 +"2"); //32
+
+//console.log(true); //true
+//console.log(+true); //1
+//console.log(true+) //error
+//console.log(+"") //0
+
+let num1 ,num2 ,num3 
+num1 =num2 = num3 = 2+2
+console.log(num2) //4
+
+let gameCounter = 100
+//gameCounter ++;
+console.log(gameCounter) //101
+++gameCounter;
+console.log(gameCounter) //101
