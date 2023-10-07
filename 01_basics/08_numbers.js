953ffe294170f84559b9dc3ee78500c7410e11c7
Anuja_Bhise <96904529+LearnerAnuja@users.noreply.github.com>
2023-10-07 18:26:47 +0000

    numbers and Math

---
+++ 01_basics/08_numbers.js
@@ -0,0 +1,26 @@
+const score = 400
+
+const balance = new Number (100)
+// console.log(balance);
+// console.log(score);
+
+// console.log(balance.toString());
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(4));//add 4 precison values
+
+const otherNum =23.895678
+
+// console.log(otherNum.toPrecision(5))
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+//#####  MATH_LIBRARY    #######//
+
+console.log(Math);
+console.log(Math.abs(-5)); //converts negative valur to positive
+console.log(Math.round(4.3)); 
+console.log(Math.ceil(4.3)); 
+console.log(Math.floor(4.3)); 
+
