ee041a9d1b4b4eaaee17096589228e44ebdc2e42
Anuja_Bhise <96904529+LearnerAnuja@users.noreply.github.com>
2023-10-09 07:33:49 +0000

    date and time

--- 01_basics/08_numbers.js
+++ 01_basics/08_numbers.js
@@ -23,4 +23,12 @@ console.log(Math.abs(-5)); //converts negative valur to positive
 console.log(Math.round(4.3)); 
 console.log(Math.ceil(4.3)); 
 console.log(Math.floor(4.3)); 
+console.log(Math.max(2,3,5,7));
+console.log(Math.min(2,3,5,7));
+console.log(Math.random());//random values between 0 to 1
+console.log((Math.random()*10)+1);//+1 will avoid values like 0.01 or 0.02
 
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1))+min ) 
